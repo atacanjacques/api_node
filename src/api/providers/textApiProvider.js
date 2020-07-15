21c115c1141d4e721fe9881dedfc5f180759c102
atacanjacques <atacan.jacques@gmail.com>
2020-07-15 12:53:56 +0200

    Replace request by axios

--- src/api/providers/textApiProvider.js
+++ src/api/providers/textApiProvider.js
@@ -1,19 +1,18 @@
-const request = require('request');
-const baseUrl = "https://www.randomtext.me/api";
+const axios = require('axios');
+const baseUrl = "https://loripsum.net/api";
 
 exports.getRandomText = () => {
-  // https://www.randomtext.me/api/lorem
+  // https://loripsum.net/api/plaintext
 
   return new Promise((resolve, reject) => {
 
-    request(baseUrl + '/lorem', (error, response, body) => {
-      try {
-        body = JSON.parse(body)
-        resolve(body.text_out)
-      } catch (e) {
-        console.log(e);
-        reject(false);
-      }
+    axios.get(baseUrl + '/plaintext', {responseType: "text"})
+    .then((response) =>{
+      resolve(response.data);
+    })
+    .catch((error) =>{
+      console.log(error);
+      reject(false);
     })
 
   })
